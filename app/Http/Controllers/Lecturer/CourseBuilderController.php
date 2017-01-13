a7a80523b586f5a51af13a6f81932f3fd20336c6
Josh <jharing10@Joshs-MacBook-Pro.local>
2017-01-13 20:54:27 +0200

    Added NPM dependencies

--- app/Http/Controllers/Lecturer/CourseBuilderController.php
+++ app/Http/Controllers/Lecturer/CourseBuilderController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Lecturer;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class CourseBuilderController extends Controller
-{
+class CourseBuilderController extends Controller {
     //
 }
