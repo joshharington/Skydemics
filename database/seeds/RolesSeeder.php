a910b40ef93aa246f2b3e8d0858e6bb719713a2f
Josh <jharing10@Joshs-MacBook-Pro.local>
2017-01-13 16:19:30 +0200

    RoleSeeder

---
+++ database/seeds/RolesSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use \Ultraware\Roles\Models\Role;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+
+        $roles = ['Super Admin', 'Admin', 'Lecturer', 'Student'];
+
+        for($i = 0; $i < count($roles); $i++) {
+            Role::create([
+                'name' => $roles[$i],
+                'slug' => str_slug($roles[$i])
+            ]);
+        }
+
+    }
+}
